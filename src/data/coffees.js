7c5d39e4868af5d81eda1bfd2a8c14d3caecee94
ChanonNon <nonchak42@gmail.com>
2022-09-05 01:00:25 +0700

    Create coffees.js

---
+++ src/data/coffees.js
@@ -0,0 +1,34 @@
+const coffees = [
+    {
+        title : "Americano",
+        thumbnailUrl : "/images/Americano.jpg",
+        // fullUrl : "/images/tattoo-01-large.jpg"
+    },
+    {
+        title : "Cappuccino",
+        thumbnailUrl : "/images/Cappuccino.jpg",
+        // fullUrl : "/images/tattoo-02-large.jpg"
+    },
+    {
+        title : "Cocoa",
+        thumbnailUrl : "/images/Cocoa.jpg",
+        // fullUrl : "/images/tattoo-03-large.jpg"
+    },
+    {
+        title : "Espresso",
+        thumbnailUrl : "/images/Espresso.jpg",
+        // fullUrl : "/images/tattoo-04-large.jpg"
+    },
+    {
+        title : "Latte",
+        thumbnailUrl : "/images/Latte.jpg",
+        // fullUrl : "/images/tattoo-05-large.jpg"
+    },
+    {
+        title : "Mocha",
+        thumbnailUrl : "/images/Mocha.jpg",
+        // fullUrl : "/images/tattoo-06-large.jpg"
+    }
+];
+
+export default coffees;
