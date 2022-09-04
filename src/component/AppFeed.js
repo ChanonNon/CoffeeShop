d9d7baf94b3f281964ac75de5bad1ddf4e5c5ea9
ChanonNon <nonchak42@gmail.com>
2022-09-05 01:00:35 +0700

    Create AppFeed.js

---
+++ src/component/AppFeed.js
@@ -0,0 +1,12 @@
+import "./AppFeed.css";
+
+function AppFeed(){
+    return(
+        <div className="app-feed">
+            {/* <img className="app-feed-logo" src="/images/2.png"/> */}
+            <img alt="app-feed" className="app-feed-image" src="/images/Coffee-BG-Feed.jpg" />
+        </div>
+    );
+}
+
+export default AppFeed; 
